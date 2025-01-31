09001bd9c9fffdaade939d6bf1ad5d4e493f535e
m0xn <moon0xcoder@gmail.com>
2025-01-31 07:06:17 +0100

    Add changes made to the app until now

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+function handleLeftClick(listItem) {
+	const name = listItem.innerHTML.substring(0, listItem.innerHTML.indexOf("<"));
+	const parentId = listItem.parentNode.id;
+
+	if (parentId == "c1-list-items") {
+		if (c1SelectedPlayers.includes(name)) {
+			listItem.style.background = getComputedStyle(listItem).getPropertyValue("--c1-player-panel-entry-selected");
+			c1SelectedPlayers.push(name);
+		}
+		else {
+			listItem.style.background = getComputedStyle(listItem).getPropertyValue("--c1-player-panel-entry-bg")
+			c1SelectedPlayers = c1SelectedPlayers.filter(n => n != name);
+		}
+	}
+	else {
+		if (c2SelectedPlayers.includes(name)) {
+			listItem.style.background = getComputedStyle(listItem).getPropertyValue("--c2-player-panel-entry-selected");
+			c2SelectedPlayers.push(name);
+		}
+		else {
+			listItem.style.background = getComputedStyle(listItem).getPropertyValue("--c2-player-panel-entry-bg")
+			c2SelectedPlayers = c2SelectedPlayers.filter(n => n != name);
+		}
+	}
+}
+
+function handleRightClick(listItem) {
+	const name = listItem.innerHTML.substring(0, listItem.innerHTML.indexOf("<"));
+	const parentId = listItem.parentNode.id;
+
+	listItem.remove();
+
+	if (parentId == "c1-list-items") {
+		c1PlayerList = c1PlayerList.filter(player => player != name);
+		localStorage.setItem(parentId, JSON.stringify(c1PlayerList));
+	} else {
+		c2PlayerList = c2PlayerList.filter(player => player != name);
+		localStorage.setItem(parentId, JSON.stringify(c2PlayerList));
+	}
+}
+
 function createPlayer(name) {
 	const entry = document.createElement("li");
 	entry.className = "list-entry";
@@ -7,6 +48,11 @@ function createPlayer(name) {
 	speedPar.innerHTML = "0";
 
 	entry.appendChild(speedPar);
+	entry.addEventListener("click", () => { handleLeftClick(entry); })
+	entry.addEventListener("contextmenu", e => {
+		e.preventDefault();
+		handleRightClick(entry);
+	})
 	return entry;
 }
 
@@ -28,18 +74,21 @@ function resetPlayers(itemsList, playerList, LSKey) {
 }
 
 const c1List = document.querySelector("ul#c1-list-items");
-const c1PlayerList = JSON.parse(localStorage.getItem(c1List.id)) || [];
+let c1PlayerList = JSON.parse(localStorage.getItem(c1List.id)) || [];
 const c1AddPlayerBtn = document.querySelector("section#c1-list-mod-ctrls>button");
 const c1NamePlayerField = document.querySelector("section#c1-list-mod-ctrls>input");
 
 const c2List = document.querySelector("ul#c2-list-items");
-const c2PlayerList = JSON.parse(localStorage.getItem(c2List.id)) || [];
+let c2PlayerList = JSON.parse(localStorage.getItem(c2List.id)) || [];
 const c2AddPlayerBtn = document.querySelector("section#c2-list-mod-ctrls>button");
 const c2NamePlayerField = document.querySelector("section#c2-list-mod-ctrls>input");
 
 const eraseC1ListBtn = document.querySelector("button#erase-c1");
 const eraseC2ListBtn = document.querySelector("button#erase-c2");
 const eraseAllBtn = document.querySelector("button#erase-all");
+
+let c1SelectedPlayers = [];
+let c2SelectedPlayers = [];
 
 for (const player of c1PlayerList) c1List.appendChild(createPlayer(player));
 for (const player of c2PlayerList) c2List.appendChild(createPlayer(player));
