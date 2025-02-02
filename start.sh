29d474cd24483b1e92f0d631dfe6ff363f656b8c
m0xn <moon0xcoder@gmail.com>
2025-02-02 18:14:30 +0100

    Modified scripts to use python's system executable

--- start.sh
+++ start.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-source ./venv/bin/activate
-python -m http.server 8000 >/dev/null &
-exo-open --launch WebBrowser localhost:8000
+PYTHON_EXEC=python3
+
+python3 -V >/dev/null
+if [[ ! $? -eq 0 ]]; then
+	echo "python3 not found... Trying with 'python'"
+	python -V >/dev/null
+	if [[ ! $? -eq 0 ]]; then
+		echo "python not found... You must have Python ver.3 installed on your system to execute this script"
+		return 1
+	else
+		PYTHON_EXEC=python
+	fi
+fi
+
+$PYTHON_EXEC -m http.server 8080 &
+exo-open --launch WebBrowser localhost:8080
